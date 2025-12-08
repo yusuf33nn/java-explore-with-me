b68bcf1f7154c932da1c8f283f8f709a588fedeb
y_muradov <surgeon33nn@yandex.ru>
2025-12-08 16:56:02 +0300

    fix build

--- ewm-main-service/src/main/java/ru/practicum/ewm/EwmServiceApplication.java
+++ ewm-main-service/src/main/java/ru/practicum/ewm/EwmServiceApplication.java
@@ -3,7 +3,7 @@ package ru.practicum.ewm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"ru.practicum.ewm", "ru.practicum.statsclient"})
 public class EwmServiceApplication {
 
     public static void main(String[] args) {
